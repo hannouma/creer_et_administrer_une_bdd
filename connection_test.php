40f8c2bfbe18d7bb4465ff67bb1e5e69d50417b6
hannouma <hannouma@hotmail.com>
2024-02-26 12:43:15 +0100

    Add connection_test.php for database connection testing

---
+++ connection_test.php
@@ -0,0 +1,25 @@
+<?php
+$dsn = 'mysql:host=localhost;dbname=cinemaBDD';
+$username = 'user.php';
+$password = 'Cinem@d4t4B@$e';
+
+try{
+    $pdo = new PDO($dsn, $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    //Récupèrer les utilisateurs
+    $query = "SELECT * FROM users";
+    $stmt = $pdo->query($query);
+    $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    //Afficher les utilisateurs
+    foreach ($users as $user){
+        echo "ID : ".$user['id']."<br>";
+        echo "Username : ".$user['username']."<br>";
+        echo "email : ".$user['email']."<br>";
+        echo "<br>";
+    }
+}
+catch (PDOException $e){
+    echo 'Erreue de connexion à la bdd'.$e->getMessage();
+}
